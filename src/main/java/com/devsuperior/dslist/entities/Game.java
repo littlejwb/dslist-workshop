192defb83382f166189f8ba43e1cbd6f35972b08
Juliana Ricardo <szric.ju@gmail.com>
2023-05-11 17:07:03 -0300

    feat: game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 public class Game {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
 
